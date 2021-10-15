e96c0705283880db30eef102cf1f257ced642509
grantbrooks18 <grantbrooks18@gmail.com>
2021-10-14 20:34:14 -0400

    -early work on portfolio page

---
+++ script.js
@@ -0,0 +1,11 @@
+function portfolio() {
+  location.replace("portfolio.html")
+}
+
+function home() {
+  location.replace("main.html")
+}
+
+function soulfull_redirect() {
+  location.replace("https://www.soulfull.ca/")
+}
