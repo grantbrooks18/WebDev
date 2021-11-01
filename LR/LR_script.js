4faf0a2c9279e070a5f1c09743d75aa9716d9d9f
grantbrooks18 <grantbrooks18@gmail.com>
2021-10-31 21:32:59 -0400

    Additions to account page

--- LR/LR_script.js
+++ LR/LR_script.js
@@ -6,3 +6,22 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+function welcome_message(){
+  var x = document.getElementById("snackbar");
+  x.className = "show";
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+
+  maxLength = 3;
+  for (var i = 0; i < maxLength; i++) {
+      for (var j = 0; j < 2; j++) {
+          tmp = 'cell' + i + j;
+          document.getElementById(tmp).innerHTML = randomnum();
+        }
+  }
+}
+
+
+function randomnum(){
+  return Math.floor(Math.random() * 10000 + 1);
+}
